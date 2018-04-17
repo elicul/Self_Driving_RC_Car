2009cace32ddf0657b89740f9afe24be4652b725
elicul <enzo.licul@yahoo.com>
2018-04-17 15:21:38 +0200

    Repo cleaned

--- utils/sigmoid_gradient.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env python
-"""Derivative of the Sigmoid Function"""
-from utils.sigmoid import sigmoid
-
-def sigmoid_gradient(x_value):
-    """Return value of derivative of Sigmoid function"""
-    return sigmoid(x_value) * (1 - sigmoid(x_value))
