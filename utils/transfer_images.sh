4502d956f7dfbee57e4c5c0e65d5e1248c7aa088
elicul <enzo.licul@yahoo.com>
2018-04-23 20:58:21 +0200

    Transfer script update

--- utils/transfer_images.sh
+++ utils/transfer_images.sh
@@ -1,3 +1,3 @@
 #!bin/bash
-rsync -avz --progress -e ssh pi@192.168.0.187:/home/pi/Self_Driving_RC_Car/images /home/elicul/Documents/GitRepos/Self_Driving_RC_Car/images
+rsync -avz --progress -e ssh pi@192.168.0.187:/home/pi/Self_Driving_RC_Car/images /home/elicul/Documents/GitRepos/Self_Driving_RC_Car
 echo "Transfer of images successfully finished!"
