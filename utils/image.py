9e53e2d40b2a98822e22cb3ba8ab730c3184c180
elicul <enzo.licul@yahoo.com>
2018-04-23 22:57:34 +0200

    Update

--- utils/image.py
+++ utils/image.py
@@ -1,10 +1,11 @@
 """Image helpers"""
 import time
+import configuration
 from PIL import Image
 
 def save_image_with_direction(stream, direction):
     """Save image"""
     stream.seek(0)
     image = Image.open(stream)
-    image = image.crop((0, PICAMERA_RESOLUTION_HEIGHT / 2, PICAMERA_RESOLUTION_WIDTH, PICAMERA_RESOLUTION_HEIGHT))
+    image = image.crop((0, configuration.PICAMERA_RESOLUTION_HEIGHT / 2, configuration.PICAMERA_RESOLUTION_WIDTH, configuration.PICAMERA_RESOLUTION_HEIGHT))
     image.save('images/' + direction + '/image%s.jpg' % ("-" + direction + "-"+ str(time.time())), format="JPEG")
