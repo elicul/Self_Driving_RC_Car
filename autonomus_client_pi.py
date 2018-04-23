8bc572eea2bcb9cd14bdc2f79282c92fd83ad086
elicul <enzo.licul@yahoo.com>
2018-04-23 23:40:54 +0200

    Update

--- autonomus_client_pi.py
+++ autonomus_client_pi.py
@@ -43,7 +43,7 @@ def Main():
                 stream = io.BytesIO()
                 image.save(stream, 'JPEG')
                 stream.seek(0)                
-                image_base64 = base64.b64encode(stream.read()).decode()
+                image_base64 = base64.b64encode(stream.read())
                                 
                 image_len = struct.pack('!i', len(image_base64))
                 client_socket.send(image_len)
