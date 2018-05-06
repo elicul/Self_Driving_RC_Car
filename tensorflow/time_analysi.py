e09a81f30a3a33525b02765379903c67b0f4675c
elicul <enzo.licul@yahoo.com>
2018-05-06 10:53:49 +0200

    Update

--- tensorflow/time_analysi.py
+++ tensorflow/time_analysi.py
@@ -3,6 +3,12 @@ import matplotlib.pyplot as plt
 with open('transfer_time_logs.txt') as infile:
     transfer_time = [[int(i) for i in line.strip().split('/n')] for line in infile]
 
+suma = 0
+for t in transfer_time:
+    suma = suma + t[0]
+
+print(suma / len(transfer_time))
+
 transfer_number = range(len(transfer_time))
 
 plt.plot(transfer_number, transfer_time, color='g')
